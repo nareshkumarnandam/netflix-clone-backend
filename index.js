29314df4349e31f96c71c967ac590f45ba56a5ea
Naresh Kumar Nandam <naresh@vizmo.in>
2024-06-20 21:37:45 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import databaseConnection from "./utils/database.js";
+import cookieParser from "cookie-parser";
+import userRoute from '../backend/routes/userRoute.js';
+import cors from 'cors';
+
+databaseConnection();
+
+dotenv.config({
+    path:".env"
+})
+
+const app = express();
+
+//middlewares
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(cookieParser());
+const corsOptions = {
+    origin:'http://localhost:3000',
+    credentials:true
+}
+app.use(cors(corsOptions));
+
+//apis
+
+app.use("/api/v1/user", userRoute);
+
+app.listen(process.env.PORT, () => {
+ console.log(`Server is up and listening at port-${process.env.PORT}`);
+})
